2c152136bc7879ff691eb15f4892812acf04a3ae
Ayrton Wong <[ayrtonwongg@gmail.com]>
2024-04-17 19:34:14 -0400

    Changed things

--- backend/apigetter.py
+++ backend/apigetter.py
@@ -1,2 +1,27 @@
-def accessAPI():
+import json
+import requests
+
+class jsonfilegrabber:
+  def __init__(self, token):
+    self.token = token
+
+  def fetch_api_config(self, url):
+    headers = {'Authorization': f'Bearer {self.token}'}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return json.loads(response.text)
+        else:
+            print(f"Failed to fetch API configuration. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred while fetching API configuration: {e}")
+    return None
+
+def accessjsonfile(token, apiurl):
     
+  filegrabber = jsonfilegrabber(token)
+
+  api_config = filegrabber.fetch_api_config(apiurl)
+
+
+
---
+++ frontend/spotify.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import os
+import base64
+from 
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_base64), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic" + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    data = {"grant-type": "client_credentials"}
+    result = post(url, headerrs=headers, data=data)
