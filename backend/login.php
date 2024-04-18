5e2d1a1b39b4d5803f6b4512f8a4f92661267e48
Your Namedwadwadadfwadwadhawhjduwahdgit config --global user.email you@example.comgit config --global user.name Your <drohi2026@gmail.com>
2024-04-18 01:40:17 -0400

    server

--- backend/login.php
+++ backend/login.php
@@ -1,21 +1,29 @@
 <?php
-	$username = $_POST['username'];
-	$password = $_POST['password'];
+$servername = "192.168.20.145";
+$username = "derek@localhost";
+$password = "password";
+$database = "hackathon1";
 
-	// Database connection
-	$conn = new mysqli('localhost','root','','test');
-	if($conn->connect_error){
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into registration(username, password) 
-        values(?, ?)");
-		$stmt->bind_param("ss", $username, $password);
-		$execval = $stmt->execute();
-		echo $execval;
-		echo "Registration successfully...";
-		$stmt->close();
-		$conn->close();
-	}
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// SQL query
+$sql = "SELECT * FROM your_table";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
 ?>
-
